c13eb2c72ca708cc284e335cf7e74a23141ff4d9
Michael Lazebny <miskadl09@gmail.com>
2025-05-29 08:27:34 +0200

    fix: remove view of cart

--- feature/cart/lib/src/widget/cart_screen.dart
+++ feature/cart/lib/src/widget/cart_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cart/src/dependencies/cart_dependencies.dart';
 import 'package:flutter/material.dart';
-import 'package:navigation_api/cart_navigator.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -9,20 +8,9 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final dependencies = CartDependencies.of(context);
 
-    return CartView(cartNavigator: dependencies.cartNavigator);
-  }
-}
-
-class CartView extends StatelessWidget {
-  const CartView({super.key, required this.cartNavigator});
-
-  final CartNavigator cartNavigator;
-
-  @override
-  Widget build(BuildContext context) {
     return Center(
       child: TextButton(
-        onPressed: () => cartNavigator.goToCartDetails(context),
+        onPressed: () => dependencies.cartNavigator.goToCartDetails(context),
         child: Text('Open Details'),
       ),
     );
