c2b56abb870ffdec7a1f179f974bb76566318ec7
Michael Lazebny <miskadl09@gmail.com>
2025-05-29 08:26:57 +0200

    fix: merge screen and view

--- feature/listing/lib/src/widget/listing_screen.dart
+++ feature/listing/lib/src/widget/listing_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:listing/src/dependencies/listing_dependencies.dart';
-import 'package:settings/settings.dart';
 
 class ListingScreen extends StatelessWidget {
   const ListingScreen({super.key});
@@ -8,23 +7,13 @@ class ListingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final container = ListingDependencies.of(context);
-    return ListingView(settingsController: container.settingsController);
-  }
-}
 
-class ListingView extends StatelessWidget {
-  const ListingView({required this.settingsController, super.key});
-
-  final SettingsController settingsController;
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ListenableBuilder(
-          listenable: settingsController,
+          listenable: container.settingsController,
           builder: (context, child) {
-            return Text('Settings: ${settingsController.settings.toString()}');
+            return Text('Settings: ${container.settingsController.settings.toString()}');
           },
         ),
       ),
